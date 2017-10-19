bab98d59684ab3b2fcc0d6f887bbcbc022d05ae6
aokhotnikov <hunterov1984@gmail.com>
2017-10-19 12:49:38 +0300

    add earning maket

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,6 +10,22 @@ export class HomePage {
   isAndroid: boolean = false;
   toggle: string = 'gross';
   amount: number = 52307.04;
+  monthes: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  quarters: string[] = ['January, February, March', 'April, May, June', 'July, August, September', 'October, November, December'];
+  quartersData: any[] = [
+    {
+      year: 2017,
+      quarters: [423523, 23423.23, 3123.00, 1233.22]
+    },
+    {
+      year: 2016,
+      quarters: [423523, 23423.23, 3123.00, 1233.22]
+    },
+    {
+      year: 2015,
+      quarters: [423523, 23423.23, 3123.00, 1233.22]
+    }
+  ];
 
   constructor(platform: Platform) {
     this.isAndroid = platform.is('android');
