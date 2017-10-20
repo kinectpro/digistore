2d736a38b44bc3c241001d063a3fa5e0d934f9c2
aokhotnikov <hunterov1984@gmail.com>
2017-10-20 18:09:53 +0300

    add get Totals to earningService

--- src/pages/earning/earning.ts
+++ src/pages/earning/earning.ts
@@ -9,16 +9,25 @@ import { EarningService } from '../../providers/earning-service';
 })
 export class EarningPage {
   segment: string = "total";
-  toggle: string = 'gross';
-  amount: number = 52307.04;
+  toggle: string = 'brutto';
   monthes: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   quarters: string[] = ['January, February, March', 'April, May, June', 'July, August, September', 'October, November, December'];
   monthlyData: any[];
   quarterlyData: any[];
   yearlyData: any[];
+  totalData: any = {
+    netto: {},
+    brutto: {}
+  };
 
   constructor(public navCtrl: NavController, public eServ: EarningService) {
-    // this.someObject = this.eServ.getTotal();
+
+    console.log('Init EarningPage');
+
+    this.eServ.getTotal().then(
+      res => this.totalData = res,
+      err => console.log(err)
+    );
     // this.monthlyData = this.eServ.getMonthlyData();
     this.quarterlyData = this.eServ.getQuarterlyData();
     this.yearlyData = this.eServ.getYearlyData();
@@ -27,5 +36,6 @@ export class EarningPage {
   goToTransaction(period: string): void {
     this.navCtrl.push(TransactionsPage, { period: period });
   }
+
 
 }
--- src/providers/earning-service.ts
+++ src/providers/earning-service.ts
@@ -3,17 +3,55 @@
  */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Settings } from '../config/settings';
+import { AuthService } from './auth-service';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
 export class EarningService {
 
-    constructor(public http: HttpClient) {
-        console.log('Init EarningServiceProvider');
+    statsSalesSummary: {[key: string]: any};
+
+    constructor(public http: HttpClient, public auth: AuthService) {
+      console.log('Init EarningServiceProvider');
     }
 
-    getTotal() {
+    getStatsSalesSummary(): Observable<{[key: string]: any}> {
+      return this.http.get(Settings.BASE_URL + this.auth.apiKey + '/json/statsSalesSummary?language=en');
+    }
 
+    getTotal(): Promise<any> {
+      return new Promise((resolve, reject) => {
+        this.getStatsSalesSummary().subscribe(
+          res => {
+            if (res.result === 'success') {
+              this.statsSalesSummary = {
+                netto: {
+                  total: res.data.for.all.amounts[""].total_netto_amount + 25,
+                  today: res.data.for.day.amounts[""].total_netto_amount + 25,
+                  week: res.data.for.week.amounts[""].total_netto_amount + 25,
+                  month: res.data.for.month.amounts[""].total_netto_amount + 25,
+                  year: res.data.for.year.amounts[""].total_netto_amount + 25
+                },
+                brutto: {
+                  total: res.data.for.all.amounts[""].total_brutto_amount,
+                  today: res.data.for.day.amounts[""].total_brutto_amount,
+                  week: res.data.for.week.amounts[""].total_brutto_amount,
+                  month: res.data.for.month.amounts[""].total_brutto_amount,
+                  year: res.data.for.year.amounts[""].total_brutto_amount
+                }
+              };
+              resolve(this.statsSalesSummary);
+            }
+            else {
+              reject(res.message);
+            }
+          },
+          err => {
+            reject(err);
+          });
+      });
     }
 
     getMonthlyData() {
@@ -21,20 +59,20 @@ export class EarningService {
     }
 
     getQuarterlyData() {
-        return [
-            {
-                year: 2017,
-                quarters: [423523, 23423.23, 3123.00, 1233.22]
-            },
-            {
-                year: 2016,
-                quarters: [423523, 23423.23, 3123.00, 1233.22]
-            },
-            {
-                year: 2015,
-                quarters: [423523, 23423.23, 3123.00, 1233.22]
-            }
-        ]
+      return [
+        {
+          year: 2017,
+          quarters: [423523, 23423.23, 3123.00, 1233.22]
+        },
+        {
+          year: 2016,
+          quarters: [423523, 23423.23, 3123.00, 1233.22]
+        },
+        {
+          year: 2015,
+          quarters: [423523, 23423.23, 3123.00, 1233.22]
+        }
+      ]
     }
 
     getYearlyData() {
