24e505037e44e65956f388c8956a22db15d41936
aokhotnikov <hunterov1984@gmail.com>
2017-11-06 11:42:59 +0200

    add validateTicket to TicketService

--- src/providers/ticket-service.ts
+++ src/providers/ticket-service.ts
@@ -5,11 +5,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Settings } from '../config/settings';
 import { AuthService } from './auth-service';
+import { TicketParams } from '../models/params';
 
 @Injectable()
 export class TicketService {
-
-  params: {[key: string]: any} = {};
 
   constructor(public http: HttpClient, public auth: AuthService) {
     console.log('Init TicketServiceProvider');
@@ -20,9 +19,10 @@ export class TicketService {
       this.http.get(`${Settings.BASE_URL}${this.auth.apiKey}/json/getEticketSettings?language=en`).subscribe(
         (res: any) => {
           if (res.result === 'success') {
-            this.params.templates = res.data.eticket_templates;
-            this.params.locations = res.data.eticket_locations;
-            resolve(this.params);
+            resolve({
+              templates: res.data.eticket_templates,
+              locations: res.data.eticket_locations
+            });
           }
           else {
             reject(res.message);
@@ -32,31 +32,32 @@ export class TicketService {
       );
     });
   }
-  //
-  // getCurrencies(): Promise<any> {
-  //   return new Promise((resolve, reject) => {
-  //     this.http.get(`${Settings.BASE_URL}${this.auth.apiKey}/json/listCurrencies?language=en`, {
-  //       params: new HttpParams().set('no-spinner', 'true')
-  //     }).subscribe(
-  //       (res: any) => {
-  //         if (res.result === 'success') {
-  //           this.currencies = res.data.map(obj => obj.code);
-  //           resolve(res.data.map(obj => {
-  //             return {
-  //               'name': obj.name,
-  //               'code': obj.code,
-  //               'symbol': obj.symbol
-  //             }
-  //           }));
-  //         }
-  //         else {
-  //           reject(res.message);
-  //         }
-  //       },
-  //       err => reject(err)
-  //     );
-  //   });
-  // }
+
+  validateTicket(params: TicketParams): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let ticketParams = new HttpParams();
+      ticketParams = ticketParams.append('location_id', params.location_id.key);
+      ticketParams = ticketParams.append('template_id', params.template_id.key);
+      ticketParams = ticketParams.append('eticket_id', params.eticket_id);
+      ticketParams = ticketParams.append('date', params.date);
+      this.http.get(`${Settings.BASE_URL}${this.auth.apiKey}/json/validateEticket?language=en`, {
+        params: ticketParams
+      }).subscribe(
+        (res: any) => {
+          if (res.result === 'success') {
+            resolve({
+              'status': res.data.status,
+              'msg': res.data.msg
+            });
+          }
+          else {
+            reject(res.message);
+          }
+        },
+        err => reject(err)
+      );
+    });
+  }
 
 }
 
