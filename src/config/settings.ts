380f2b4970119893525ffc9ca996c235524d1a2e
Maksym <glimpseofmetal@gmail.com>
2017-12-12 16:56:45 +0200

    Update settings & minor update for autocomplete

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,21 +1,26 @@
 export class Settings {
   // Dev version:
-  // static SITE_URL = 'https://digitest24.de';
-  // static BASE_URL = 'https://digitest24.de/api/call/';
-  // static API_KEY = '187-zEEF6WeuTtVXA27WE7pwEFCWaO6N9mNs578C4VKT';
+  static SITE_URL = 'https://digitest24.de';
+  static BASE_URL = 'https://digitest24.de/api/call/';
+  static API_KEY = '187-zEEF6WeuTtVXA27WE7pwEFCWaO6N9mNs578C4VKT';
 
   // Dev:
-  static SITE_URL = 'https://digitest24.de';
-  static BASE_URL = 'api/';
-  static API_KEY = '187-zEEF6WeuTtVXA27WE7pwEFCWaO6N9mNs578C4VKT';
+  // static SITE_URL = 'https://digitest24.de';
+  // static BASE_URL = 'api/';
+  // static API_KEY = '187-zEEF6WeuTtVXA27WE7pwEFCWaO6N9mNs578C4VKT';
 
   // Production version:
   // static SITE_URL = 'https://www.digistore24.com';
   // static BASE_URL = 'https://www.digistore24.com/api/call/';
   // static API_KEY = '25273-ZgMA3G5FMlhTvLa6WzAST9DwDVPMi6u65ecTepLX';
 
-  static ONE_SIGNAL_APPID = "14745566-6bc8-4857-ba8b-25187a918f21";
-  static GOOGLE_PROJECT_NUMBER = "project-254132814053";
+  // Dev:
+  // static ONE_SIGNAL_APPID = "14745566-6bc8-4857-ba8b-25187a918f21";
+  // static GOOGLE_PROJECT_NUMBER = "project-254132814053";
+
+  // Live:
+  static ONE_SIGNAL_APPID = "93e3f5c1-5342-418e-9bf2-d79d0036d6f1";
+  static GOOGLE_PROJECT_NUMBER = "project-829938099620";
 }
 
 
